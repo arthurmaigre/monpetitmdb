0386c4bd0fe635de830a25341088eb67415e75b9
Arthur Maigre <arthur.maigre@gmail.com>
2026-04-02 17:22:02 +0200

    fix: expose BREVO vars via next.config.ts env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    BREVO_API_KEY: process.env.BREVO_API_KEY,
+    BREVO_SENDER_EMAIL: process.env.BREVO_SENDER_EMAIL,
+  },
 };
 
 export default nextConfig;
