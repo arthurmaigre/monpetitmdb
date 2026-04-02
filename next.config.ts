2eae8124e9a15e5a64a77fa6c92f634d9740235e
Arthur Maigre <arthur.maigre@gmail.com>
2026-04-02 17:36:18 +0200

    fix: retire env block next.config (figeait BREVO a undefined au build)

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
-// Force rebuild pour variables Brevo
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    BREVO_API_KEY: process.env.BREVO_API_KEY,
-    BREVO_SENDER_EMAIL: process.env.BREVO_SENDER_EMAIL,
-  },
 };
 
 export default nextConfig;
